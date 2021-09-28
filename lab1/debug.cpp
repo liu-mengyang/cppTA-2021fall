997f146cdceaa19502318046a0d00067548c9072
梦阳 <ethan_lmy@163.com>
2021-09-28 23:00:39 +0800

    initial commit

---
+++ lab1/debug.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main()
+{
+    int i = 0;
+    for (i = 0; i < 10; i++) {
+        std::cout << "Hello, time(s)" << i << std::endl;
+    }
+}
---
+++ lab1/test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int a = 0;
+    int b = 0;
+    int sum = 0;
+
+    std::cout << "enter a:";
+    std::cin >> a;
+    std::cout << "enter b:";
+    std::cin >> b;
+
+    sum = a + b;
+    std::cout <<"sum is " << sum << std::endl;
+}
