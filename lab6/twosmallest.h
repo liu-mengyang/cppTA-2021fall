e91e6eacb787233726b70e8fb6671b9276ee8f0b
梦阳 <ethan_lmy@163.com>
2021-12-07 23:37:23 +0800

    bug fixed

--- lab6/twosmallest.h
+++ lab6/twosmallest.h
@@ -9,13 +9,19 @@ int TwoSmallest(T a, T b, T c, T &s1, T &s2){
         s1 = b;
         s2 = a;
     }
-    if ( c < s2){
-        if ( c < s1){
+    else if ( b == a){
+        if (c < b)
             s1 = c;
-            s2 = a;
+    }
+    else{
+        if ( c < s2){
+            if ( c < s1){
+                s1 = c;
+                s2 = a;
+            }
+            else
+                s2 = c;
         }
-        else
-            s2 = c;
     }
     if (s1 == s2)
         return 1;
