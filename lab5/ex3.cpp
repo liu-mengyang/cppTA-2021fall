b13250171f6e5f82e11e375087b7616a3d028755
梦阳 <ethan_lmy@163.com>
2021-12-09 19:27:20 +0800

    bug fixed

--- lab5/ex3.cpp
+++ lab5/ex3.cpp
@@ -13,7 +13,7 @@ bool is_prime(int value){
 }
 
 int main(){
-    int count = 1;
+    int count = 0;
     cout << "The prime numbers from 1 to 10000 are:" << endl;
     for (int i = 2; i <= 10000; i++){
         if (is_prime(i)){
